c875369a47896ca10a814d2b1847f4a0bda727b8
Kathryn Hodge <blondiebytes@gmail.com>
2022-08-23 18:55:32 +0000

    Init challenge

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,21 @@
 package com.linkedin.javacodechallenges;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+public class App {
+
+    public static double calculateWaterBill(double gallonsUsage) {
+        // TODO: Implement method
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many gallons of water did you " +
+                "use this month?");
+        double usage = scanner.nextDouble();
+        System.out.println("Your water bill is " +
+                calculateWaterBill(usage));
+        scanner.close();
     }
 }
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -1,20 +1,23 @@
 package com.linkedin.javacodechallenges;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void calculateWaterBill() {
+        assertEquals(22.74, App.calculateWaterBill(1800), 0);
+        assertEquals(22.74, App.calculateWaterBill(2244), 0);
+        assertEquals(26.64, App.calculateWaterBill(2245), 0);
+        assertEquals(26.64, App.calculateWaterBill(2992), 0);
+        assertEquals(46.14, App.calculateWaterBill(6000), 0);
+        assertEquals(53.94, App.calculateWaterBill(8000), 0);
+
+        assertEquals(18.84, App.calculateWaterBill(0), 0);
+        assertEquals(18.84, App.calculateWaterBill(1496), 0);
+        assertEquals(22.74, App.calculateWaterBill(1497), 0);
+        assertEquals(18.84, App.calculateWaterBill(-20), 0);
     }
 }
