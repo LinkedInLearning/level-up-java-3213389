ba45198228834fca2cb7fea32dea028c7f0f1a42
Kathryn Hodge <blondiebytes@gmail.com>
2022-08-23 16:06:26 +0000

    Init challenge

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,21 @@
 package com.linkedin.javacodechallenges;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+public class App {
+
+    public static boolean isPasswordComplex(String password) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a password: ");
+        String userInput = scanner.nextLine();
+        System.out.println("Is the password complex? "
+                + isPasswordComplex(userInput));
+
+        scanner.close();
     }
 }
