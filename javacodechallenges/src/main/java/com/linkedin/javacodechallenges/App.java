d4aad877b0537a6acf4a863cf14b7c6d653f4104
Kathryn Hodge <blondiebytes@gmail.com>
2022-08-23 19:18:13 +0000

    Init challenge

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,9 @@
 package com.linkedin.javacodechallenges;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
     }
 }
