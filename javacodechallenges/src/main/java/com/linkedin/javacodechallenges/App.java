3d2e443a9522c933687e66ee9447520647f58a6b
Kathryn Hodge <blondiebytes@gmail.com>
2022-08-24 15:36:34 +0000

    init challenge

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,14 @@
 package com.linkedin.javacodechallenges;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.List;
+
+public class App {
+    public static double calculateAverageChangeInvested(List<Double> purchases) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        List<Double> purchases = List.of(12.38, 38.29, 5.27, 3.21);
+        System.out.println(calculateAverageChangeInvested(purchases));
     }
 }
