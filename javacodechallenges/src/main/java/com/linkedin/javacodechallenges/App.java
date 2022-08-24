2992384352f90f14fe2ae80c0b77f1bb8092a133
Kathryn Hodge <blondiebytes@gmail.com>
2022-08-24 12:12:04 +0000

    Add tests

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,16 +1,18 @@
 package com.linkedin.javacodechallenges;
 
-public class App 
-{
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+
+public class App {
     public static LocalDate calculateHundredDaysFromNow(LocalDate today) {
         Period hundredDays = Period.ofDays(100);
         return today.plus(hundredDays);
     }
 
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         LocalDate today = LocalDate.now(ZoneId.of("America/New_York"));
         System.out.println("100 days from now is... " +
-            calculateHundredDaysFromNow(today));
+                calculateHundredDaysFromNow(today));
     }
 }
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -1,20 +1,25 @@
 package com.linkedin.javacodechallenges;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDate;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void calculateHundredDaysFromNow() {
+        LocalDate d1 = LocalDate.of(2020, 1, 1);
+        assertEquals(LocalDate.of(2020, 4, 10),
+                App.calculateHundredDaysFromNow(d1));
+
+        LocalDate d2 = LocalDate.of(2020, 11, 6);
+        assertEquals(LocalDate.of(2021, 2, 14),
+                App.calculateHundredDaysFromNow(d2));
+
+        LocalDate d3 = LocalDate.of(2022, 11, 8);
+        assertEquals(LocalDate.of(2023, 2, 16),
+                App.calculateHundredDaysFromNow(d3));
     }
 }
