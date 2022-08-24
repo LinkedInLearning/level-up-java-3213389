8cb8aa8da1cb7fe47f4e64a73550c9dee5e9b87a
Kathryn Hodge <blondiebytes@gmail.com>
2022-08-24 12:03:33 +0000

    Init challenge

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,13 @@
 package com.linkedin.javacodechallenges;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
+    // Create function to calculate the date that's 
+    // 100 days from now
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("100 days from now is... " 
+            /* add function call */);
     }
 }
