e3444d19a313638524b3e849974af4cedf198a90
Kathryn Hodge <blondiebytes@gmail.com>
2022-08-24 13:27:36 +0000

    Init challenge

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,30 @@
 package com.linkedin.javacodechallenges;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Map;
+import java.util.Scanner;
+
+public class App {
+    public static final Map<Character, Integer> letterPoints = Map.ofEntries(Map.entry('A', 1),
+            Map.entry('B', 3), Map.entry('C', 3), Map.entry('D', 2), Map.entry('E', 1),
+            Map.entry('F', 4), Map.entry('G', 2), Map.entry('H', 4), Map.entry('I', 1),
+            Map.entry('J', 8), Map.entry('K', 5), Map.entry('L', 1), Map.entry('M', 3),
+            Map.entry('N', 1), Map.entry('O', 1), Map.entry('P', 3), Map.entry('Q', 10),
+            Map.entry('R', 1), Map.entry('S', 1), Map.entry('T', 1), Map.entry('U', 1),
+            Map.entry('W', 4), Map.entry('V', 4), Map.entry('X', 8), Map.entry('Y', 4),
+            Map.entry('Z', 10));
+
+    public static int wordScoreCalculator(String word) {
+        return 0;
     }
+
+    public static void main(String[] args) {
+        System.out.println("Enter a word and we'll tell you how many " +
+                "points it will earn!");
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        System.out.println("Your word " + word + " will earn "
+                + wordScoreCalculator(word));
+        sc.close();
+    }
+
 }
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -1,20 +1,39 @@
 package com.linkedin.javacodechallenges;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void scrabbleWordCalculator() {
+        assertEquals(10, App.wordScoreCalculator("very"));
+        assertEquals(12, App.wordScoreCalculator("zoo"));
+        assertEquals(28, App.wordScoreCalculator("maximize"));
+        assertEquals(17, App.wordScoreCalculator("exercise"));
+        assertEquals(27, App.wordScoreCalculator("jukebox"));
+        assertEquals(26, App.wordScoreCalculator("flapjack"));
+        assertEquals(25, App.wordScoreCalculator("squeeze"));
+        assertEquals(11, App.wordScoreCalculator("school"));
+        assertEquals(9, App.wordScoreCalculator("ex"));
+        assertEquals(12, App.wordScoreCalculator("common"));
+    }
+
+    @Test
+    public void scrabbleWordCalculator_edgeCases() {
+        assertEquals(0, App.wordScoreCalculator(""));
+        assertEquals(0, App.wordScoreCalculator("          "));
+        assertEquals(0, App.wordScoreCalculator("     12-183     "));
+
+        assertEquals(10, App.wordScoreCalculator("v2ery"));
+        assertEquals(10, App.wordScoreCalculator("Very"));
+        assertEquals(10, App.wordScoreCalculator("VeRy"));
+        assertEquals(10, App.wordScoreCalculator("VERY"));
+        assertEquals(10, App.wordScoreCalculator("ver!y"));
+        assertEquals(10, App.wordScoreCalculator("ver!y"));
+        assertEquals(10, App.wordScoreCalculator("  ve ry"));
+        assertEquals(10, App.wordScoreCalculator("  ve ry"));
+        assertEquals(7, App.wordScoreCalculator("dadd"));
     }
 }
