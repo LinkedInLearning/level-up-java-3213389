eae9bef06a399776ff2ed62c6486ff2378584542
Kathryn Hodge <blondiebytes@gmail.com>
2022-08-26 14:58:45 +0000

    Implement challenge

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,14 +1,52 @@
 package com.linkedin.javacodechallenges;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void redactTextFile(String fileName,
-                                      String[] redactedWordsArray) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
+import org.apache.commons.lang3.StringUtils;
+
+public class App {
+    public static void redactTextFile(String fileName,
+            String[] redactedWordsArray) {
+        if (!fileName.contains(".txt")) {
+            System.out.println("This is not a text file.");
+            return;
+        }
+
+        try {
+            File originalFile = new File(fileName);
+            BufferedReader originalFileReader = new BufferedReader(new FileReader(originalFile));
+
+            File redactedFile = new File("redacted-" +
+                    fileName);
+            BufferedWriter redactedFileWriter = new BufferedWriter(
+                    new FileWriter(redactedFile));
+
+            String currentLine = originalFileReader.readLine();
+
+            while (currentLine != null) {
+                for (String redactedWord : redactedWordsArray) {
+                    currentLine = StringUtils
+                            .replaceIgnoreCase(currentLine,
+                                    redactedWord, "REDACTED");
+                }
+
+                redactedFileWriter.append(currentLine).append("\n");
+                currentLine = originalFileReader.readLine();
+            }
+
+            originalFileReader.close();
+            redactedFileWriter.close();
+
+        } catch (IOException e) {
+            System.out.println("Trouble reading or writing to file"
+                    + e);
+        }
     }
 
     public static void main(String[] args) {
@@ -27,5 +65,7 @@ public class App
         String[] redactedWordsList = redactedWords.split(",");
 
         redactTextFile(fileName, redactedWordsList);
+
+        scanner.close();
     }
 }
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -1,20 +1,150 @@
 package com.linkedin.javacodechallenges;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+
+        private File originalFile;
+        private File redactedFile;
+        private BufferedReader redactedFileReader;
+        private BufferedReader originalFileReader;
+
+        private final ByteArrayOutputStream printOut = new ByteArrayOutputStream();
+
+        @Before
+        public void setUpEach() {
+                System.setOut(new PrintStream(printOut));
+        }
+
+        @After
+        public void cleanUpEach() {
+                System.setOut(System.out);
+        }
+
+        @Test
+        public void redactTextFile_ensureRedaction() {
+
+                try {
+                        App.redactTextFile("sample.txt",
+                                        new String[] { "sally", "Kat", "ready to go" });
+
+                        redactedFile = new File("redacted-sample.txt");
+                        redactedFileReader = new BufferedReader(
+                                        new FileReader(redactedFile));
+
+                        redactedFileReader.lines().forEach(line -> {
+                                assertFalse(line.contains("sally"));
+                                assertFalse(line.contains("Kat"));
+                                assertFalse(line.contains("ready to go"));
+                        });
+                } catch (IOException e) {
+                        System.out.println(e);
+                }
+        }
+
+        @Test
+        public void redactTextFile_ensureSameRemainingContents() {
+
+                try {
+
+                        App
+                                        .redactTextFile("sample.txt",
+                                                        new String[] { "sally", "Kat",
+                                                                        "ready to go" });
+
+                        originalFile = new File("sample.txt");
+                        originalFileReader = new BufferedReader(
+                                        new FileReader(originalFile));
+
+                        redactedFile = new File("redacted-sample.txt");
+                        redactedFileReader = new BufferedReader(
+                                        new FileReader(redactedFile));
+
+                        List<String> redactedLines = originalFileReader.lines()
+                                        .map(line -> line.replace("sally",
+                                                        "REDACTED")
+                                                        .replace("kat",
+                                                                        "REDACTED")
+                                                        .replace("Kat",
+                                                                        "REDACTED")
+                                                        .replace("Sally",
+                                                                        "REDACTED")
+                                                        .replace("ready to go",
+                                                                        "REDACTED"))
+                                        .collect(Collectors.toList());
+
+                        redactedLines.forEach(line -> {
+                                try {
+                                        assertEquals(line, redactedFileReader.readLine());
+                                } catch (IOException e) {
+                                        System.out.println("ERROR");
+                                }
+                        });
+
+                } catch (IOException e) {
+                        System.out.println(e);
+                }
+        }
+
+        @Test
+        public void redactTextFile_ensureSameNumberOfLines() {
+
+                try {
+                        App.redactTextFile("sample.txt",
+                                        new String[] { "sally", "Kat" });
+
+                        originalFile = new File("sample.txt");
+                        originalFileReader = new BufferedReader(
+                                        new FileReader(originalFile));
+
+                        redactedFile = new File("redacted-sample.txt");
+                        redactedFileReader = new BufferedReader(
+                                        new FileReader(redactedFile));
+
+                        List<String> redactedLines = originalFileReader.lines()
+                                        .map(line -> line.replace("sally",
+                                                        "REDACTED")
+                                                        .replace("kat",
+                                                                        "REDACTED")
+                                                        .replace("Kat",
+                                                                        "REDACTED")
+                                                        .replace("Sally",
+                                                                        "REDACTED")
+                                                        .replace("ready to go",
+                                                                        "REDACTED"))
+                                        .collect(Collectors.toList());
+
+                        originalFileReader.reset();
+                        assertEquals(originalFileReader.lines().toArray().length,
+                                        redactedLines.size());
+
+                } catch (IOException e) {
+                        System.out.println(e);
+                }
+        }
+
+        @Test
+        public void redactTextFile_wrongFileType() {
+
+                App.redactTextFile("sample.jpg",
+                                new String[] { "sally", "Kat", "ready to go" });
+                File tempFile = new File("redacted-sample.jpg");
+                assertFalse(tempFile.exists());
+                assertEquals("This is not a text file.\n",
+                                printOut.toString());
+        }
 }
