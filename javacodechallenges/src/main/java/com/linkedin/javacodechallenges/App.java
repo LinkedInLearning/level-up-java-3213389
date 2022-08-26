5c664735f2c9ce73aa3e04c43796b2fb9e92c8fb
Kathryn Hodge <blondiebytes@gmail.com>
2022-08-26 14:46:07 +0000

    Init challenge

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -6,8 +6,26 @@ package com.linkedin.javacodechallenges;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void redactTextFile(String fileName,
+                                      String[] redactedWordsArray) {
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What file would you like to " +
+                "redact information from?");
+        String fileName = scanner.nextLine();
+
+        System.out.println("What words would you like to redact? " +
+                "Separate each word or phrase with a comma. " +
+                "If you phrase includes punctuation, include " +
+                "that in your input.");
+        String redactedWords = scanner.nextLine();
+        String[] redactedWordsList = redactedWords.split(",");
+
+        redactTextFile(fileName, redactedWordsList);
     }
 }
