f64d6612147b1d3d0dad2757292be6ff4ba145b0
Kathryn Hodge <blondiebytes@gmail.com>
2022-08-26 12:06:24 +0000

    Implement revealResults

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/TeamUtils.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/TeamUtils.java
@@ -1,7 +1,10 @@
 package com.linkedin.javacodechallenges;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class TeamUtils {
 
@@ -17,5 +20,43 @@ public class TeamUtils {
 
   public static void revealResults(List<Team> teams) {
 
+    if (teams.size() == 0 || teams.stream()
+        .allMatch(team -> team.getScores().isEmpty())) {
+      System.out.println("The game hasn't started yet.");
+      return;
+    }
+
+    TreeMap<Integer, List<Team>> scoreTeamsMap = teams.stream()
+        .collect(Collectors.groupingBy(Team::sumTotalScore,
+            TreeMap::new, Collectors.toList()));
+    Iterator<Integer> scoreResultsIterator = scoreTeamsMap.descendingKeySet()
+        .stream()
+        .iterator();
+
+    System.out.println("Now for the results, the WINNER is...");
+    announceResult(scoreResultsIterator.next(), scoreTeamsMap);
+
+    while (scoreResultsIterator.hasNext()) {
+      System.out.println("Then we have... ");
+      announceResult(scoreResultsIterator.next(), scoreTeamsMap);
+    }
   }
+
+  private static void announceResult(int score,
+      TreeMap<Integer, List<Team>> scoreTeamsMap) {
+
+    List<Team> playersWithScore = scoreTeamsMap.get(score);
+
+    if (playersWithScore.size() > 1) {
+      System.out.println("It's a TIE!");
+    }
+
+    playersWithScore
+        .forEach(team -> System.out.println("With " + score +
+            " points, it's team " + team.getPlayer1() + " and " +
+            team.getPlayer2() + "!"));
+
+    System.out.println();
+  }
+
 }
