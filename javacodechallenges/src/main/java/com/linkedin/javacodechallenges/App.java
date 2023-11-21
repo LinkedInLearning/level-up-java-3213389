f17dd2112b2ff3c73cec5fde9b93cea42d1d9510
anushacharya <48857063+anushacharya@users.noreply.github.com>
2023-11-21 15:10:01 +0000

    Password challenege

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,50 @@
 package com.linkedin.javacodechallenges;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class App {
+
+    public static boolean isPasswordComplex(String password) {
+    
+        // if(password.length()>= 6){
+            
+        //     return Pattern.matches("[\\w\\W\\s]*", password);
+        //     }
+        // return false;
+
+        return password.length()>=6 && password.matches(".*\\d.*")&& password.matches(".*[a-z].*")
+        && password.matches(".*[A-Z].*");
+        // if(password.length()<6){
+        //     return false;
+        // }
+
+        // boolean hasLowercaseLetter = false;
+        // boolean hasUppercaseLetter = false;
+        // boolean hasNumber = false;
+
+        // for( int i=0; i < password.length() || !hasLowercaseLetter && !hasUppercaseLetter && !hasNumber;i++){
+        //   char current = password.charAt(i);
+        //   if(Character.isDigit(current)){
+        //     hasNumber = true;
+        //   }
+        //   else if(Character.isLowerCase(current)){
+        //     hasLowercaseLetter = true;
+        //   }  else if(Character.isUpperCase(current)){
+        //     hasUppercaseLetter = true;
+        //   } 
+        // }
+        // return hasNumber && hasLowercaseLetter && hasUppercaseLetter ;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a password: ");
+        String userInput = scanner.nextLine();
+        System.out.println("Is the password complex? "
+                + isPasswordComplex(userInput));
+
+        scanner.close();
     }
 }
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -1,20 +1,32 @@
 package com.linkedin.javacodechallenges;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void isPasswordComplex_true() {
+        assertTrue(App.isPasswordComplex("Happy12"));
+        assertTrue(App.isPasswordComplex("1SarahL ength"));
+        assertTrue(App.isPasswordComplex("Password12"));
+        assertTrue(App.isPasswordComplex("2PassW@ord1"));
+        assertTrue(App.isPasswordComplex("01Sall#y9"));
+    }
+
+    @Test
+    public void isPasswordComplex_false() {
+        assertFalse(App.isPasswordComplex("12aB "));
+        assertFalse(App.isPasswordComplex("Ab112"));
+        assertFalse(App.isPasswordComplex("happy"));
+        assertFalse(App.isPasswordComplex("happy_1"));
+        assertFalse(App.isPasswordComplex("p assword12"));
+        assertFalse(App.isPasswordComplex("Sur18"));
+        assertFalse(App.isPasswordComplex(""));
     }
 }
